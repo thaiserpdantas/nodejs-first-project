8e1dc37b6674d65b377ef68501b472a6ff5a10b0
Thaise Dantas <thaiserpdantas@gmail.com>
2020-07-26 21:15:36 -0300

    Config build

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' })
+    return response.json({ message: 'Hello Node' })
 })
 
 const port = 3333;
