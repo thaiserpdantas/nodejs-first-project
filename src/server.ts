b550be494e02820897c69f1666d6960d94f231d4
Thaise Dantas <thaiserpdantas@gmail.com>
2020-08-01 19:43:57 -0300

    Add eslint and prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import express from 'express'
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello Node' })
-})
+app.use(express.json());
+
+app.get('/', (request, response) => response.json({ message: 'Hello Node' }));
 
 const port = 3333;
 app.listen(3333, () => {
-    console.log('✨ Server started on port: ', port)
-})
+    console.log('✨ Server started on port: ', port);
+});
