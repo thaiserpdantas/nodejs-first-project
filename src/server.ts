3721a3c9f878569c86467c8c24f6caf7264f8bfd
Thaise Dantas <thaiserpdantas@gmail.com>
2020-08-01 20:10:51 -0300

    Fix routes

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import routes from './routes';
 
 const app = express();
 
-app.use(express.json());
+app.use(routes);
 
 app.get('/', (request, response) => response.json({ message: 'Hello Node' }));
 
